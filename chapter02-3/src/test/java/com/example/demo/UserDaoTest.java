2dc4b45e3d81af4da553a82c92bf8ba5853e44d4
minwan <ansatgol@gmail.com>
2018-05-15 00:10:34 +0900

    addAndGet() 테스트보완

--- chapter02-3/src/test/java/com/example/demo/UserDaoTest.java
+++ chapter02-3/src/test/java/com/example/demo/UserDaoTest.java
@@ -30,19 +30,24 @@ public class UserDaoTest {
 
         User user1 = new User("gyumee", "박성철", "springno1");
         User user2 = new User("leegw700", "이길원", "springno2");
-        User user3 = new User("bumjin", "박범진", "springno3");
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
         dao.add(user1);
-        assertThat(dao.getCount(), is(1));
-
         dao.add(user2);
         assertThat(dao.getCount(), is(2));
 
-        dao.add(user3);
-        assertThat(dao.getCount(), is(3));
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
+
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
+
+
+
 
     }
 
