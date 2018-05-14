de0dd29ae567bec879d845fa4fb6e3ea1cac102e
minwan <ansatgol@gmail.com>
2018-05-15 00:22:43 +0900

    2.3 테스트를 위한 개발자 프리임워크 JUnit

--- chapter02-3/src/test/java/com/example/demo/UserDaoTest.java
+++ chapter02-3/src/test/java/com/example/demo/UserDaoTest.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.user.DaoFactory;
 import com.example.demo.user.User;
 import com.example.demo.user.UserDao;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
@@ -25,12 +26,21 @@ public class UserDaoTest {
     @Autowired
     private ApplicationContext applicationContext;
 
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @Before
+    public void setUp() throws Exception {
+        dao = applicationContext.getBean("userDao",UserDao.class);
+        user1 = new User("gyumee", "박성철", "springno1");
+        user2 = new User("leegw700", "이길원", "springno2");
+        user3 = new User("bumjin", "박범진", "springno3");
+    }
+
     @Test
     public void addAndGet() throws ClassNotFoundException, SQLException {
-        final UserDao dao = applicationContext.getBean("userDao",UserDao.class);
-
-        final User user1 = new User("gyumee", "박성철", "springno1");
-        final User user2 = new User("leegw700", "이길원", "springno2");
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
@@ -51,13 +61,28 @@ public class UserDaoTest {
 
     @Test(expected = EmptyResultDataAccessException.class)
     public void getUserFailure() throws SQLException, ClassNotFoundException {
-        final UserDao dao = applicationContext.getBean("userDao",UserDao.class);
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
         dao.get("unkonw_id");
+    }
 
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 
     public static void main(String[] args){
