f7991eea2d34cfebadba6e126ef41180bc4c9f83
minwan <ansatgol@gmail.com>
2018-05-15 22:39:14 +0900

    3.2 변하는것과 변하지 않는것으로 분리

---
+++ chapter03-2/src/main/java/com/example/demo/user/DeleteAllStatement.java
@@ -0,0 +1,14 @@
+package com.example.demo.user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteAllStatement implements StatementStrategy {
+
+    @Override
+    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        return ps;
+    }
+}
---
+++ chapter03-2/src/test/java/com/example/demo/UserDaoTest.java
@@ -0,0 +1,97 @@
+package com.example.demo;
+
+import com.example.demo.user.DaoFactory;
+import com.example.demo.user.JdbcContext;
+import com.example.demo.user.User;
+import com.example.demo.user.UserDao;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserDaoTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    private UserDao dao;
+    private JdbcContext jdbcContext;
+    private User user1;
+    private User user2;
+    private User user3;
+
+
+
+    @Before
+    public void setUp() throws Exception {
+        dao = applicationContext.getBean("userDao",UserDao.class);
+        jdbcContext = applicationContext.getBean("jdbcContext",JdbcContext.class);
+        user1 = new User("gyumee", "박성철", "springno1");
+        user2 = new User("leegw700", "이길원", "springno2");
+        user3 = new User("bumjin", "박범진", "springno3");
+    }
+
+    @Test
+    public void addAndGet() throws ClassNotFoundException, SQLException {
+
+        jdbcContext.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
+
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
+
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void getUserFailure() throws SQLException, ClassNotFoundException {
+
+        jdbcContext.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.get("unkonw_id");
+    }
+
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+
+        jdbcContext.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+    }
+
+    public static void main(String[] args){
+        JUnitCore.main("com.example.demo.UserDaoTest");
+    }
+
+}
