dd038246b8c57b083daeb47b07a0f518f2301af0
minwan <ansatgol@gmail.com>
2018-05-19 21:32:00 +0900

    6.2 Proxy 추가

--- chapter06-2/src/test/java/com/example/demo/proxy/HelloTargetTest.java
+++ chapter06-2/src/test/java/com/example/demo/proxy/HelloTargetTest.java
@@ -8,9 +8,9 @@ import static org.junit.Assert.*;
 public class HelloTargetTest {
     @Test
     public void simpleProxy() {
-        Hello hello = new HelloTarget();
-        assertThat(hello.sayHello("Toby"), is("Hello Toby"));
-        assertThat(hello.sayHi("Toby"), is("Hi Toby"));
-        assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+        Hello hello =  new HelloUppercase(new HelloTarget());
+        assertThat(hello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(hello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(hello.sayThankYou("Toby"), is("THANK YOU TOBY"));
     }
 }
